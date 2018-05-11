c48602a74ddee87837fc97d011357f9432412b6c
pawelplodzpl <pawel.kapusta@p.lodz.pl>
2018-05-11 10:54:05 +0200

    repairing the NEZ shaders sample (replace files in NEZ.Samples original repo to use

---
+++ Nez.Samples/Scenes/Empty Scene/BasicScene.cs
@@ -0,0 +1,86 @@
+﻿using Nez.Sprites;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Nez.Samples
+{
+
+    public class myComponent : Component, IUpdatable
+    {
+
+        float timeSinceLastReset = 0.0f;
+        float timeToWait = 0.1f;
+
+
+        public void update()
+        {
+
+            timeSinceLastReset += Time.deltaTime;
+            if (timeSinceLastReset > timeToWait)
+            {
+                var player = Nez.Core.scene.findEntity("moon");
+                var material = player.getComponent<Sprite>().material;
+                var effect = material.effect;
+                var parameters = effect.Parameters;
+                parameters["_attenuation"].SetValue(Nez.Random.nextFloat(1.0f));
+                parameters["_linesFactor"].SetValue(100* Nez.Random.nextFloat(6.0f));
+                timeSinceLastReset = 0.0f;
+
+            }
+
+        }
+
+    }
+
+    public class mySecondComponent : Component, IUpdatable
+    {
+        int timer = 0;
+
+        public void update()
+        {
+            timer++;
+            var player = Nez.Core.scene.findEntity("moon");
+            var material = player.getComponent<Sprite>().material;
+            var effect = material.effect;
+            var parameters = effect.Parameters;
+            parameters["Timer"].SetValue((float)timer / 10.0f);
+
+        }
+    }
+
+
+    [SampleScene( "Basic Scene", 9999, "Scene with a single Entity. The minimum to have something to show" )]
+	public class BasicScene : SampleScene
+	{
+		public override void initialize()
+		{
+			base.initialize();
+
+
+            //var test = content.loadNezEffect<ScanlinesEffect>();
+            //test.attenuation = 1.0f;
+            //test.linesFactor = 20.0f;
+
+            var test = content.loadNezEffect<WobbleEffect>();
+
+            var material = new Material(test);
+
+            // default to 1280x720 with no SceneResolutionPolicy
+            setDesignResolution( 1280, 720, Scene.SceneResolutionPolicy.None );
+			Screen.setSize( 1280, 720 );
+
+			var moonTex = content.Load<Texture2D>( Content.Shared.moon );
+			var playerEntity = createEntity( "moon", new Vector2( Screen.width / 2, Screen.height / 2 ) );
+            var testSprite = new Sprite(moonTex);
+            testSprite.material = material;
+			playerEntity.addComponent( testSprite );
+
+            //playerEntity.addComponent<myComponent>();
+            playerEntity.addComponent<mySecondComponent>();
+
+
+        }
+	}
+}
+
---
+++ Nez/Nez.Portable/Graphics/Effects/WobbleEffect.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Nez
+{
+	public class WobbleEffect : Effect
+	{
+        /// <summary>Timer/GameTime for animating the effect</summary>
+        public float Timer
+        {
+			get { return _timer; }
+			set
+			{
+				if(_timer != value )
+				{
+                    _timer = value;
+					_timerParam.SetValue(_timer);
+				}
+			}
+		}
+
+        /// <summary>Refraction Amount.</summary>
+        /// <minValue>20</minValue>
+        /// <maxValue>60</maxValue>
+        /// <defaultValue>50</defaultValue>
+        public float Refracton
+        {
+            get { return _refraction; }
+            set
+            {
+                if (_refraction != value)
+                {
+                    _refraction = value;
+                    _refractionParam.SetValue(_refraction);
+                }
+            }
+        }
+
+        /// <summary>Vertical trough</summary>
+        /// <minValue>20</minValue>
+        /// <maxValue>30</maxValue>
+        /// <defaultValue>23</defaultValue>
+        public float VerticalTroughWidth
+        {
+            get { return _verticalThroughWidth; }
+            set
+            {
+                if (_verticalThroughWidth != value)
+                {
+                    _verticalThroughWidth = value;
+                    _verticalThroughWidthParam.SetValue(_verticalThroughWidth);
+                }
+            }
+        }
+
+
+        /// <summary>Center X of the Zoom.</summary>
+        /// <minValue>20</minValue>
+        /// <maxValue>30</maxValue>
+        /// <defaultValue>23</defaultValue>
+        public float Wobble2
+        {
+            get { return _wobble2; }
+            set
+            {
+                if (_wobble2 != value)
+                {
+                    _wobble2 = value;
+                    _wobble2Param.SetValue(_wobble2);
+                }
+            }
+        }
+
+        float _timer = 0.0f;
+        float _refraction = 50.0f;
+        float _verticalThroughWidth = 23.0f;
+        float _wobble2 = 23.0f;
+
+
+		EffectParameter _timerParam;
+        EffectParameter _verticalThroughWidthParam;
+        EffectParameter _wobble2Param;
+        EffectParameter _refractionParam;
+
+
+        public WobbleEffect() : base( Core.graphicsDevice, EffectResource.wobbleBytes )
+		{
+
+            _timerParam = Parameters["Timer"];
+            _timerParam.SetValue(_timer);
+
+
+            _refractionParam = Parameters["Refracton"];
+            _refractionParam.SetValue(_refraction);
+
+            _verticalThroughWidthParam = Parameters["VerticalTroughWidth"];
+            _verticalThroughWidthParam.SetValue(_verticalThroughWidth);
+
+
+            _wobble2Param = Parameters["Wobble2"];
+            _wobble2Param.SetValue(_wobble2);
+        }
+	}
+}
+
